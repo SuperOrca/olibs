a48aca8652df1fcef9e54453df9db6fda6789813
SuperOrca <shreyasbhar@gmail.com>
2023-08-20 13:29:53 -0400

    new stuff

--- src/main/kotlin/com/github/SuperOrca/olibs/Utils.kt
+++ src/main/kotlin/com/github/SuperOrca/olibs/Utils.kt
@@ -3,8 +3,12 @@ package com.github.SuperOrca.olibs
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.TextDecoration
 import net.kyori.adventure.text.minimessage.MiniMessage
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
 import org.apache.commons.lang3.time.DurationFormatUtils
+import org.bukkit.Sound
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+import java.math.BigDecimal
+import java.math.RoundingMode
 import java.text.DecimalFormat
 import java.util.*
 import kotlin.math.ln
@@ -15,71 +19,140 @@ private val serializer = MiniMessage.builder()
     .build()
 private val endings = arrayOf("k", "M", "B", "T", "Q", "QT", "S", "SP", "O")
 private val smallCapitalMap = mapOf(
-    'a' to '\u1d00', 'b' to '\u0299', 'c' to '\u1d04', 'd' to '\u1d05', 'e' to '\u1d07',
-    'f' to '\ua730', 'g' to '\u0262', 'h' to '\u029c', 'i' to '\u026a', 'j' to '\u1d0a',
-    'k' to '\u1d0b', 'l' to '\u029f', 'm' to '\u1d0d', 'n' to '\u0274', 'o' to '\u1d0f',
-    'p' to '\u1d18', 'q' to '\ua731', 'r' to '\u0280', 's' to '\ua731', 't' to '\u1d1b',
-    'u' to '\u1d1c', 'v' to '\u1d20', 'w' to '\u1d21', 'x' to '\u1d22', 'y' to '\u028f',
-    'z' to '\u1d23'
+    'a' to 'ᴀ',
+    'b' to 'ʙ',
+    'c' to 'ᴄ',
+    'd' to 'ᴅ',
+    'e' to 'ᴇ',
+    'f' to 'ғ',
+    'g' to 'ɢ',
+    'h' to 'ʜ',
+    'i' to 'ɪ',
+    'j' to 'ᴊ',
+    'k' to 'ᴋ',
+    'l' to 'ʟ',
+    'm' to 'ᴍ',
+    'n' to 'ɴ',
+    'o' to 'ᴏ',
+    'p' to 'ᴘ',
+    'q' to 'ǫ',
+    'r' to 'ʀ',
+    's' to 's',
+    't' to 'ᴛ',
+    'u' to 'ᴜ',
+    'v' to 'ᴠ',
+    'w' to 'ᴡ',
+    'x' to 'x',
+    'y' to 'ʏ',
+    'z' to 'ᴢ'
 )
 
 fun component(text: String): Component {
     return serializer.deserialize(text)
 }
 
-fun smallCaps(input: String): String {
-    val outputBuilder = StringBuilder()
-    for (char in input) {
+fun String.small(): String {
+    val output = StringBuilder()
+    for (char in this) {
         if (char in 'a'..'z') {
-            outputBuilder.append(smallCapitalMap[char])
+            output.append(smallCapitalMap[char])
         } else {
-            outputBuilder.append(char)
+            output.append(char)
         }
     }
-    return outputBuilder.toString()
+    return output.toString()
 }
 
-fun compact(number: Long): String {
-    if (number < 1000L) return "" + number
-    val exp = (ln(number.toDouble()) / ln(1000.0)).toInt()
-    return "%.1f%s".format(number / 1000.0.pow(exp.toDouble()), endings[exp - 1])
+fun Long.compact(): String {
+    if (this < 1000L) return "" + this
+    val exp = (ln(this.toDouble()) / ln(1000.0)).toInt()
+    return "%.1f%s".format(this / 1000.0.pow(exp.toDouble()), endings[exp - 1])
 }
 
-fun compact(number: Double): String {
-    if (number < 1000.0) return "%.0f".format(number)
-    val exp = (ln(number) / ln(1000.0)).toInt()
-    return "%.1f%s".format(number / 1000.0.pow(exp.toDouble()), endings[exp - 1])
+fun Double.compact(): String {
+    if (this < 1000.0) return "%.0f".format(this)
+    val exp = (ln(this) / ln(1000.0)).toInt()
+    return "%.1f%s".format(this / 1000.0.pow(exp.toDouble()), endings[exp - 1])
 }
 
-fun compact(number: Number?): String? {
-    return if (number is Long) compact(number) else if (number is Double) compact(number) else null
+fun Long.format(): String {
+    return DecimalFormat("#,###").format(this)
 }
 
-fun format(number: Long): String {
-    return DecimalFormat("#,###").format(number)
+fun Double.format(): String {
+    return DecimalFormat("#,###").format(this)
 }
 
-fun format(number: Int): String {
-    return DecimalFormat("#,###").format(number.toLong())
-}
-
-fun format(number: Double): String {
-    return DecimalFormat("#,###").format(number)
-}
-
-fun formatDuration(duration: Long): String {
-    return DurationFormatUtils.formatDurationWords(duration, true, false)
-}
-
-fun componentToString(component: Component): String {
-    return LegacyComponentSerializer.legacySection().serialize(component)
-}
-
-fun stringToComponent(string: String): Component {
-    return if (string.contains("&")) LegacyComponentSerializer.legacyAmpersand()
-        .deserialize(string) else return LegacyComponentSerializer.legacySection().deserialize(string)
+fun Long.formatDuration(): String {
+    return DurationFormatUtils.formatDurationWords(this, true, true)
 }
 
 fun String.title(): String =
     lowercase().split(" ")
         .joinToString(" ") { it -> it.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() } }
+
+fun Double.round(places: Int): Double {
+    return BigDecimal(this).setScale(places, RoundingMode.HALF_EVEN).toDouble()
+}
+
+fun String.parseId(): String {
+    return this.lowercase().replace("_", " ")
+}
+
+fun String.scramble(): String {
+    return toCharArray().toMutableList().shuffled().joinToString("")
+}
+
+fun ItemStack.amount(amount: Int): ItemStack {
+    val clone = this.clone()
+    clone.amount = amount
+    return clone
+}
+
+fun Player.playSound(sound: Sound, volume: Float, pitch: Float) {
+    playSound(location, sound, volume, pitch)
+}
+
+fun Int.roman(): String {
+    val romanMap = mapOf(
+        1000 to "M", 900 to "CM", 500 to "D", 400 to "CD",
+        100 to "C", 90 to "XC", 50 to "L", 40 to "XL",
+        10 to "X", 9 to "IX", 5 to "V", 4 to "IV", 1 to "I"
+    )
+
+    var remaining = this
+    val result = StringBuilder()
+
+    for ((value, symbol) in romanMap) {
+        while (remaining >= value) {
+            remaining -= value
+            result.append(symbol)
+        }
+    }
+
+    return result.toString()
+}
+
+fun Long.compactDuration(): String {
+    val seconds = (this / 1000L) % 60L
+    val minutes = (this / (1000L * 60L)) % 60L
+    val hours = (this / (1000L * 60L * 60L)) % 24L
+    val days = (this / (1000L * 60L * 60L * 24L)) % 365L
+
+    val builder = StringBuilder()
+
+    if (days > 0) {
+        builder.append("${days}d")
+    }
+    if (hours > 0) {
+        builder.append("${hours}h")
+    }
+    if (minutes > 0) {
+        builder.append("${minutes}m")
+    }
+    if (seconds > 0) {
+        builder.append("${seconds}s")
+    }
+
+    return builder.toString()
+}
